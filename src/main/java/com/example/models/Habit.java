f639c17e37f1369d15284f56cd4b42eb1a7bb759
JLao <joannie.lao@gmail.com>
2025-01-25 19:15:56 -0500

    Updating daily streak.

--- src/main/java/com/example/models/Habit.java
+++ src/main/java/com/example/models/Habit.java
@@ -9,8 +9,19 @@ public class Habit {
     int streak;
     private LocalDate lastCompleted;
 
-
-
-
-    
+    public void habitCompleted(){
+        LocalDate today = LocalDate.now();
+        if (lastCompleted != null){
+            // If the last time a habit was completed was yesterday, update the streak appropriately.
+            if (lastCompleted.plusDays(1) == today){
+                streak++;
+            }
+        }
+        // If the task has never been completed or the last time it was completed was not yesterday, update the streak appropriately. 
+        else if (lastCompleted == null || lastCompleted.plusDays(1) != today){
+            streak = 1;
+        }
+        lastCompleted = today;
+        }
+    }
 }
