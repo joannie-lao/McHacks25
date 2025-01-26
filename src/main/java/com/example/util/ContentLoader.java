4291d62fdf2d4ad62ce635dd264fe081a62c8ce3
KShubochkin <kshubochkin2022@gmail.com>
2025-01-26 02:25:04 -0500

    gson

--- src/main/java/com/example/util/ContentLoader.java
+++ src/main/java/com/example/util/ContentLoader.java
@@ -1,9 +1,14 @@
 package com.example.util;
-//import com.google.gson.Gson;
+import com.example.models.Decoration;
+import com.example.models.User;
+import com.google.gson.Gson;
 //import com.google.gson.reflect.TypeToken;
-import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class ContentLoader {
+    String json = "";
+    Gson gson = new Gson();
+    Decoration decoration = gson.fromJson(json, Decoration.class);
     
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires com.google.gson;
 
     opens com.example to javafx.fxml;
     exports com.example;
